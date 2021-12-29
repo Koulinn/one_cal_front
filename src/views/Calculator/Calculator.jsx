e27954c34d6d45ce70ad4de9bdeadc81b81bef85
Rafael Lima <drdverzola@gmail.com>
2021-12-29 06:40:24 +0000

    basic template

--- src/views/Calculator/Calculator.jsx
+++ src/views/Calculator/Calculator.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Calculator() {
-    return <div></div>
+    return <div>Calculator</div>
 }
 
 export default Calculator
--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Register() {
-    return <div></div>
+    return <div>Register</div>
 }
 
 export default Register
