9f739aad0bcc7addaf534ab3dd36b73f2403ad8e
Rafael Lima <drdverzola@gmail.com>
2021-12-29 09:52:01 +0000

    created TopNavItems

---
+++ src/components/TopNavBar/TopNavItems/TopNavItems.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Stack from '@mui/material/Stack'
+import TopNavigationItem from '../TopNavigationItem/TopNavigationItem'
+import CTAButton from '../../CTAButton/CTAButton'
+
+function TopNavItems() {
+    return (
+        <nav>
+            <Stack spacing={3} direction='row'>
+                <TopNavigationItem text='login' url={'/login'} />
+                <CTAButton text='Join now' url={'/register'} />
+            </Stack>
+        </nav>
+    )
+}
+
+export default TopNavItems
