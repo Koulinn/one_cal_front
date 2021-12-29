55674faec868f2bd0574ddbc2beae4826cd6faf9
Rafael Lima <drdverzola@gmail.com>
2021-12-29 08:18:45 +0000

    imported TopNavBar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Layout from './components/Layout/Layout'
 import { BrowserRouter } from 'react-router-dom'
 
 import './styles/App.css'
+import TopNavBar from './components/TopNavBar/TopNavBar'
 
 function App() {
     return (
         <CSSNormalize>
             <BrowserRouter>
                 <Layout>
+                    <TopNavBar />
                     <AppRoutes />
                 </Layout>
             </BrowserRouter>
