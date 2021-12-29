4662cf45856af7dcf907df1190f16032344d4e46
Rafael Lima <drdverzola@gmail.com>
2021-12-29 07:41:24 +0000

    added logo svg link

---
+++ src/assets/images_links.js
@@ -0,0 +1,6 @@
+const imageLinks = {
+    logo_svg:
+        'https://res.cloudinary.com/koulin/image/upload/v1640761796/OneKcal_web_images/OneKcal_logo_64_czfbkz.svg',
+}
+
+export default imageLinks
