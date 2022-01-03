4ae7caedd67b9dc3907584b6e00dc6bfd65f754a
Rafael Lima <drdverzola@gmail.com>
2022-01-03 07:07:32 +0000

    redirect user to calc if is logged in IndexDB

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useUser from '../../customHooks/useUser/useUser.js'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+    const { requestStatus } = useUser()
+    const navigateTo = useNavigate()
+
+    const isUserLogged = () => {
+        if (requestStatus === 'success') {
+            return navigateTo('/calc')
+        }
+    }
+
+    useEffect(() => {
+        isUserLogged()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [requestStatus])
+
     return <div>Home</div>
 }
 
