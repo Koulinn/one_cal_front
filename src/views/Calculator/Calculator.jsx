6cd9bf60cfea98e590e70d6ec6f8e0076781e286
Rafael Lima <drdverzola@gmail.com>
2022-01-04 11:28:27 +0000

    created calculator component

--- src/views/Calculator/Calculator.jsx
+++ src/views/Calculator/Calculator.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LoggedSection from '../LoggedSection/LoggedSection'
 import CalcForm from './CalcForm/CalcForm'
 import UserDashboard from './UserDashboard/UserDashboard'
 
 function Calculator() {
+    const [sendNewRequest, setSendNewRequest] = useState(false)
     return (
         <LoggedSection>
-            <CalcForm />
-            <UserDashboard />
+            <CalcForm
+                sendNewRequest={sendNewRequest}
+                setSendNewRequest={setSendNewRequest}
+            />
+            <UserDashboard sendNewRequest={sendNewRequest} />
         </LoggedSection>
     )
 }
