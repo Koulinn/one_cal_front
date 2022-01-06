976151ed845faf9ff411d437ce9153c009776172
Rafael Lima <drdverzola@gmail.com>
2022-01-06 13:44:43 +0000

    added logic to display right navBar

--- src/components/TopNavBar/TopNavBar.jsx
+++ src/components/TopNavBar/TopNavBar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Grid, Container } from '@mui/material'
 import LogoName from './LogoName/LogoName'
 import NavNotLogged from './NavNotLogged/NavNotLogged'
 import topNavBarCSS from './topNavBarCSS.js'
+import useUser from '../../customHooks/useUser/useUser.js'
+import NavLogged from './NavLogged/NavLogged'
 
 function TopNavBar() {
+    const { userData } = useUser()
+    const [user, setUser] = useState()
+
+    useEffect(() => {
+        if (userData) {
+            setUser(userData)
+        }
+    }, [userData])
     return (
         <Grid item xs={12}>
             <Container sx={topNavBarCSS}>
                 <LogoName />
-                <NavNotLogged />
+                {user ? (
+                    <NavLogged {...user} setUser={setUser} />
+                ) : (
+                    <NavNotLogged />
+                )}
             </Container>
         </Grid>
     )
