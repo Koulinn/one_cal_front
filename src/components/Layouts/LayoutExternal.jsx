a2f4c6604f48839cbef3d6b75a08769f927078a2
Rafael Lima <drdverzola@gmail.com>
2022-01-07 08:39:22 +0000

    general styling

--- src/components/Layouts/LayoutExternal.jsx
+++ src/components/Layouts/LayoutExternal.jsx
@@ -1,10 +1,21 @@
-import { Grid } from '@mui/material'
 import React from 'react'
+import { Grid } from '@mui/material'
 import layoutExternalCSS from './layoutExternalCSS'
+import { useLocation } from 'react-router-dom'
 
 function LayoutExternal({ children }) {
+    const location = useLocation()
+
+    const { pathname } = location
+
     return (
-        <Grid container id='layoutExternal' spacing={2} sx={layoutExternalCSS}>
+        <Grid
+            container
+            id='layoutExternal'
+            spacing={2}
+            sx={layoutExternalCSS}
+            className={pathname === '/' ? 'bg-home' : ''}
+        >
             {children}
         </Grid>
     )
--- src/views/Login/LoginForm/LoginForm.jsx
+++ src/views/Login/LoginForm/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Box, TextField } from '@mui/material'
+import { Button, Box, TextField, Typography } from '@mui/material'
 import useLoginForm from './useLoginForm.js'
 import LoginAlerts from './LoginAlerts'
 import GoogleButton from 'react-google-button'
@@ -16,30 +16,43 @@ function LoginForm() {
     ] = useLoginForm()
 
     return (
-        <Box component='form' onSubmit={handleSubmitLogin}>
+        <Box component='form' sx={{ mt: 2 }} onSubmit={handleSubmitLogin}>
             <TextField
                 id='email'
                 label='E-mail'
                 variant='standard'
                 fullWidth
                 type='email'
-                // value={email}
-                value={'test@mail.com'}
+                value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                sx={{ mt: 3, mb: 2 }}
             />
             <TextField
                 id='password'
                 label='Password'
                 variant='standard'
                 fullWidth
-                // value={password}
-                type={'123456A'}
+                value={password}
+                type={'password'}
                 onChange={(e) => setPassword(e.target.value)}
+                sx={{ mt: 2, mb: 3 }}
             />
             <LoginAlerts requestStatus={requestStatus} />
-            <Button type='submit'>Access Kcalculator</Button>
-            or
-            <GoogleButton onClick={handleGoogleLogin} />
+            <Button type='submit' sx={{ mt: 3 }} fullWidth>
+                Login
+            </Button>
+            <Typography
+                component={'p'}
+                variant='p'
+                textAlign={'center'}
+                sx={{ my: 5 }}
+            >
+                or
+            </Typography>
+            <GoogleButton
+                onClick={handleGoogleLogin}
+                style={{ margin: '32px auto' }}
+            />
         </Box>
     )
 }
