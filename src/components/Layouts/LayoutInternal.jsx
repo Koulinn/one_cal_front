b1e17944a25470ac8b503d9d2c7ac8878cdbb3bf
Rafael Lima <drdverzola@gmail.com>
2022-01-22 08:07:35 +0000

    fixed background color bug

--- src/components/Layouts/LayoutInternal.jsx
+++ src/components/Layouts/LayoutInternal.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Grid, Container } from '@mui/material'
 import layoutInternalCSS from './layoutInternalCSS.js'
+import useQueries from '../../customHooks/useQueries/useQueries.js'
 
 function LayoutInternal({ children }) {
+    const { isTablet } = useQueries()
     return (
         <Grid item className='layoutInternal' xs={12}>
-            <Container sx={{ backgroundColor: '#f3faf4' }}>
+            <Container
+                sx={{ backgroundColor: isTablet ? '#f3faf4' : 'transparent' }}
+            >
                 <Grid container sx={layoutInternalCSS}>
                     {children}
                 </Grid>
