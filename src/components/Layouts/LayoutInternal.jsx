557d5a9640f9c4b254bb11ad4dee8926b76c47b2
Rafael Lima <drdverzola@gmail.com>
2022-01-22 08:30:59 +0000

    added logic to fix bg on home

--- src/components/Layouts/LayoutInternal.jsx
+++ src/components/Layouts/LayoutInternal.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import { Grid, Container } from '@mui/material'
 import layoutInternalCSS from './layoutInternalCSS.js'
 import useQueries from '../../customHooks/useQueries/useQueries.js'
+import { useLocation } from 'react-router-dom'
 
 function LayoutInternal({ children }) {
     const { isTablet } = useQueries()
+    const path = useLocation()
+
+    const { pathname } = path
+
     return (
         <Grid item className='layoutInternal' xs={12}>
             <Container
-                sx={{ backgroundColor: isTablet ? '#f3faf4' : 'transparent' }}
+                sx={{
+                    backgroundColor:
+                        isTablet && pathname !== '/'
+                            ? '#f3faf4'
+                            : 'transparent',
+                }}
             >
                 <Grid container sx={layoutInternalCSS}>
                     {children}
