93e24a7ce37d93df11dfa767f6f1dc8ede77b7f2
Rafael Lima <drdverzola@gmail.com>
2022-02-03 12:59:16 +0000

    Prettier

--- src/views/Calculator/CalcForm/CalcForm.jsx
+++ src/views/Calculator/CalcForm/CalcForm.jsx
@@ -26,65 +26,68 @@ function CalcForm({ sendNewRequest, setSendNewRequest }) {
     }, [requestStatus])
 
     return (
-        <Grid
-            item
-            sm={12}
-            md={6}
-            className={isTablet ? 'simpleDashboard-mobile' : ''}
-            sx={CalcFormCSS}
-        >
-            <Box component='form' sx={{ px: 3 }} onSubmit={submitForm}>
-                <Typography
-                    component='h5'
-                    variant='h5'
-                    textAlign={'center'}
-                    sx={{ mt: 3 }}
-                >
-                    Enter meal
-                </Typography>
-                <TextField
-                    id='name'
-                    label='Name'
-                    variant='standard'
-                    fullWidth
-                    type='text'
-                    error={false}
-                    value={name}
-                    onChange={(e) => inputHandlers(e.target.value, 'name')}
-                    sx={{ mt: 3, mb: 2 }}
-                />
-                <TextField
-                    id='calories'
-                    label='Calories'
-                    variant='standard'
-                    fullWidth
-                    type='number'
-                    error={false}
-                    value={calories}
-                    onChange={(e) => inputHandlers(e.target.value, 'calories')}
-                    sx={{ mt: 2, mb: 5 }}
-                />
-                <Box fullWidth id='date-picker-wrapper' sx={{ mt: 2 }}>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DateTimePicker
-                            renderInput={(props) => <TextField {...props} />}
-                            label='Meal time'
-                            value={time_eaten}
-                            onChange={(value) =>
-                                inputHandlers(value, 'time_eaten')
-                            }
-                        />
-                    </LocalizationProvider>
-                </Box>
+      <Grid
+        item
+        sm={12}
+        md={6}
+        className={isTablet ? "simpleDashboard-mobile" : ""}
+        sx={CalcFormCSS}
+      >
+        <Box component='form' sx={{ px: 3 }} onSubmit={submitForm}>
+          <Typography
+            component='h5'
+            variant='h5'
+            textAlign={"center"}
+            sx={{ mt: 3 }}
+          >
+            Enter meal
+          </Typography>
+          <TextField
+            id='name'
+            label='Name'
+            variant='standard'
+            fullWidth
+            type='text'
+            error={false}
+            value={name}
+            onChange={(e) => inputHandlers(e.target.value, "name")}
+            sx={{ mt: 3, mb: 2 }}
+          />
+          <TextField
+            id='calories'
+            label='Calories'
+            variant='standard'
+            fullWidth
+            type='number'
+            error={false}
+            value={calories}
+            onChange={(e) => inputHandlers(e.target.value, "calories")}
+            sx={{ mt: 2, mb: 5 }}
+          />
+          <Box fullWidth id='date-picker-wrapper' sx={{ mt: 2 }}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DateTimePicker
+                renderInput={(props) => <TextField {...props} />}
+                label='Meal time'
+                value={time_eaten}
+                onChange={(value) => inputHandlers(value, "time_eaten")}
+              />
+            </LocalizationProvider>
+          </Box>
 
-                <CalcFormAlerts requestStatus={requestStatus} />
+          <CalcFormAlerts requestStatus={requestStatus} />
 
-                <Button sx={{ mt: 5, mb: 3 }} fullWidth type='submit'>
-                    Add meal
-                </Button>
-            </Box>
-        </Grid>
-    )
+          <Button
+            sx={{ mt: 5, mb: 3 }}
+            fullWidth
+            type='submit'
+            disabled={name === "" ? true : false}
+          >
+            Add meal
+          </Button>
+        </Box>
+      </Grid>
+    );
 }
 
 export default CalcForm
--- src/views/Calculator/UserDashboard/SimpleDashboard/SimpleDashboard.jsx
+++ src/views/Calculator/UserDashboard/SimpleDashboard/SimpleDashboard.jsx
@@ -8,79 +8,76 @@ function SimpleDashboard({ calorie_total_today, calorie_total_yesterday }) {
     const { showEffect } = useSimpleDashboard(calorie_total_today)
     const { isTablet } = useQueries()
     return (
-        <Grid
-            item
-            sm={12}
-            md={6}
-            className={isTablet ? 'simpleDashboard-mobile' : ''}
+      <Grid
+        item
+        sm={12}
+        md={6}
+        className={isTablet ? "simpleDashboard-mobile" : ""}
+      >
+        <Box
+          className='card-primary'
+          sx={{
+            mb: 5,
+          }}
         >
+          <Box
+            className={"card-primary-inner-wrapper " + showEffect}
+            sx={{ mb: 3, px: 4 }}
+          >
             <Box
-                className='card-primary'
-                sx={{
-                    mb: 5,
-                }}
+              sx={{
+                position: "relative",
+                width: "fit-content",
+                margin: "auto",
+              }}
+              // className={showAddIcon}
             >
-                <Box
-                    className={'card-primary-inner-wrapper ' + showEffect}
-                    sx={{ mb: 3, px: 4 }}
-                >
-                    <Box
-                        sx={{
-                            position: 'relative',
-                            width: 'fit-content',
-                            margin: 'auto',
-                        }}
-                        // className={showAddIcon}
-                    >
-                        <Typography
-                            component='h4'
-                            variant='h4'
-                            textAlign={'center'}
-                            sx={{ mt: 2, mb: 3 }}
-                        >
-                            Today
-                        </Typography>
-                    </Box>
-                    <Typography
-                        component='p'
-                        variant='p'
-                        textAlign={'center'}
-                        sx={{ mb: 1 }}
-                    >
-                        {calorie_total_today
-                            ? `${calorie_total_today} Kcal`
-                            : `0000 Kcal`}
-                    </Typography>
-                </Box>
+              <Typography
+                component='h4'
+                variant='h4'
+                textAlign={"center"}
+                sx={{ mt: 2, mb: 3 }}
+              >
+                Today
+              </Typography>
             </Box>
+            <Typography
+              component='p'
+              variant='p'
+              textAlign={"center"}
+              sx={{ mb: 1 }}
+            >
+              {calorie_total_today
+                ? `${calorie_total_today} Kcal`
+                : `0000 Kcal`}
+            </Typography>
+          </Box>
+        </Box>
 
-            <Box className='card-secondary'>
-                <Box
-                    className='card-secondary-inner-wrapper'
-                    sx={{ px: 3, pb: 2 }}
-                >
-                    <Typography
-                        component='h5'
-                        variant='h5'
-                        textAlign={'center'}
-                        sx={{ mt: 2 }}
-                    >
-                        Yesterday
-                    </Typography>
-                    <Typography
-                        component='p'
-                        variant='p'
-                        textAlign={'center'}
-                        sx={{ mt: 2 }}
-                    >
-                        {calorie_total_yesterday
-                            ? `${calorie_total_yesterday} Kcal`
-                            : 'Did you forget it again...'}
-                    </Typography>
-                </Box>
-            </Box>
-        </Grid>
-    )
+        <Box className='card-secondary'>
+          <Box className='card-secondary-inner-wrapper' sx={{ px: 3, pb: 2 }}>
+            <Typography
+              component='h5'
+              variant='h5'
+              textAlign={"center"}
+              sx={{ mt: 2 }}
+            >
+              Yesterday
+            </Typography>
+            <Typography
+              component='p'
+              variant='p'
+              textAlign={"center"}
+              sx={{ mt: 2 }}
+            >
+              {calorie_total_yesterday
+                ? `${calorie_total_yesterday} Kcal`
+                : "Did you forget it again..."}
+            </Typography>
+          </Box>
+        </Box>
+      </Grid>
+    );
 }
 
 export default SimpleDashboard
