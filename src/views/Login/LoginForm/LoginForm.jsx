1409d28f94a09574ff75eb30ee9f197f3d41edaf
Rafael Lima <drdverzola@gmail.com>
2022-02-03 11:19:47 +0000

    Disable Login btn on Oauth authentication

--- src/views/Login/LoginForm/LoginForm.jsx
+++ src/views/Login/LoginForm/LoginForm.jsx
@@ -16,45 +16,50 @@ function LoginForm() {
     ] = useLoginForm()
 
     return (
-        <Box component='form' sx={{ mt: 2 }} onSubmit={handleSubmitLogin}>
-            <TextField
-                id='email'
-                label='E-mail'
-                variant='standard'
-                fullWidth
-                type='email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                sx={{ mt: 3, mb: 2 }}
-            />
-            <TextField
-                id='password'
-                label='Password'
-                variant='standard'
-                fullWidth
-                value={password}
-                type={'password'}
-                onChange={(e) => setPassword(e.target.value)}
-                sx={{ mt: 2, mb: 3 }}
-            />
-            <LoginAlerts requestStatus={requestStatus} />
-            <Button type='submit' sx={{ mt: 3 }} fullWidth>
-                Login
-            </Button>
-            <Typography
-                component={'p'}
-                variant='p'
-                textAlign={'center'}
-                sx={{ my: 5 }}
-            >
-                or
-            </Typography>
-            <GoogleButton
-                onClick={handleGoogleLogin}
-                style={{ margin: '32px auto' }}
-            />
-        </Box>
-    )
+      <Box component='form' sx={{ mt: 2 }} onSubmit={handleSubmitLogin}>
+        <TextField
+          id='email'
+          label='E-mail'
+          variant='standard'
+          fullWidth
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          sx={{ mt: 3, mb: 2 }}
+        />
+        <TextField
+          id='password'
+          label='Password'
+          variant='standard'
+          fullWidth
+          value={password}
+          type={"password"}
+          onChange={(e) => setPassword(e.target.value)}
+          sx={{ mt: 2, mb: 3 }}
+        />
+        <LoginAlerts requestStatus={requestStatus} />
+        <Button
+          type='submit'
+          sx={{ mt: 3 }}
+          fullWidth
+          disabled={requestStatus === "success" ? true : false}
+        >
+          Login
+        </Button>
+        <Typography
+          component={"p"}
+          variant='p'
+          textAlign={"center"}
+          sx={{ my: 5 }}
+        >
+          or
+        </Typography>
+        <GoogleButton
+          onClick={handleGoogleLogin}
+          style={{ margin: "32px auto" }}
+        />
+      </Box>
+    );
 }
 
 export default LoginForm
