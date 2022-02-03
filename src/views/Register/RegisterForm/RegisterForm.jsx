a1221a17795f75294293fe3d4ffe26e6bb0eda3b
Rafael Lima <drdverzola@gmail.com>
2022-02-03 11:25:01 +0000

    Prettier

--- src/views/Register/RegisterForm/RegisterForm.jsx
+++ src/views/Register/RegisterForm/RegisterForm.jsx
@@ -1,68 +1,68 @@
-import React from 'react'
-import { Button, Box, TextField, Typography } from '@mui/material'
-import useRegisterForm from './useRegisterForm.js'
-import RegisterAlerts from './RegisterAlerts.jsx'
-import GoogleButton from 'react-google-button'
+import React, { useEffect } from "react";
+import { Button, Box, TextField, Typography } from "@mui/material";
+import useRegisterForm from "./useRegisterForm.js";
+import RegisterAlerts from "./RegisterAlerts.jsx";
+import GoogleButton from "react-google-button";
 
 function RegisterForm() {
-    const [
-        requestStatus,
-        handleRegisterForm,
-        handlePassword,
-        isValidPassword,
-        passwordMsg,
-        emailValidation,
-        isValidEmail,
-        handleGoogleSignUp,
-    ] = useRegisterForm()
+  const [
+    requestStatus,
+    handleRegisterForm,
+    handlePassword,
+    isValidPassword,
+    passwordMsg,
+    emailValidation,
+    isValidEmail,
+    handleGoogleSignUp,
+  ] = useRegisterForm();
 
-    return (
-        <Box component='form' sx={{ mt: 2 }} onSubmit={handleRegisterForm}>
-            <TextField
-                id='email'
-                label='E-mail'
-                variant='standard'
-                fullWidth
-                type='email'
-                error={!isValidEmail}
-                onChange={(e) => emailValidation(e)}
-                sx={{ mt: 3, mb: 2 }}
-            />
-            <TextField
-                id='password'
-                label='Password'
-                variant='standard'
-                fullWidth
-                type={'password'}
-                onChange={(e) => handlePassword(e.target.value)}
-                error={!isValidPassword}
-                helperText={passwordMsg}
-                sx={{ mt: 2, mb: 3 }}
-            />
-            <RegisterAlerts requestStatus={requestStatus} />
-            <Button
-                type='submit'
-                sx={{ mt: 3 }}
-                fullWidth
-                disabled={!isValidPassword || !isValidEmail}
-            >
-                Register!
-            </Button>
-            <Typography
-                component={'p'}
-                variant='p'
-                textAlign={'center'}
-                sx={{ my: 5 }}
-            >
-                or
-            </Typography>
-            <GoogleButton
-                onClick={handleGoogleSignUp}
-                label={'Register with Google'}
-                style={{ margin: '32px auto' }}
-            />
-        </Box>
-    )
+  return (
+    <Box component='form' sx={{ mt: 2 }} onSubmit={handleRegisterForm}>
+      <TextField
+        id='email'
+        label='E-mail'
+        variant='standard'
+        fullWidth
+        type='email'
+        error={!isValidEmail}
+        onChange={(e) => emailValidation(e)}
+        sx={{ mt: 3, mb: 2 }}
+      />
+      <TextField
+        id='password'
+        label='Password'
+        variant='standard'
+        fullWidth
+        type={"password"}
+        onChange={(e) => handlePassword(e.target.value)}
+        error={!isValidPassword}
+        helperText={passwordMsg}
+        sx={{ mt: 2, mb: 3 }}
+      />
+      <RegisterAlerts requestStatus={requestStatus} />
+      <Button
+        type='submit'
+        sx={{ mt: 3 }}
+        fullWidth
+        disabled={!isValidPassword || !isValidEmail}
+      >
+        Register!
+      </Button>
+      <Typography
+        component={"p"}
+        variant='p'
+        textAlign={"center"}
+        sx={{ my: 5 }}
+      >
+        or
+      </Typography>
+      <GoogleButton
+        onClick={handleGoogleSignUp}
+        label={"Register with Google"}
+        style={{ margin: "32px auto" }}
+      />
+    </Box>
+  );
 }
 
-export default RegisterForm
+export default RegisterForm;
