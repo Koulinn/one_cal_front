189ed62abd0efd72b9128721ca7741d435672e9b
Rafael Lima <drdverzola@gmail.com>
2022-02-04 16:15:49 +0000

    Split into 3 components
    TodayBox
    YesterdayBox
    MealListModal

--- src/views/Calculator/UserDashboard/SimpleDashboard/SimpleDashboard.jsx
+++ src/views/Calculator/UserDashboard/SimpleDashboard/SimpleDashboard.jsx
@@ -1,83 +1,34 @@
 import React from 'react'
-import { Grid, Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import useSimpleDashboard from './useSimpleDashboard.js'
-import useQueries from '../../../../customHooks/useQueries/useQueries.js'
+import { Grid } from "@mui/material";
+import useSimpleDashboard from "./useSimpleDashboard.js";
+import useQueries from "../../../../customHooks/useQueries/useQueries.js";
+import TodayBox from "./TodayBox.jsx";
+import YesterdayBox from "./YesterdayBox.jsx";
+import MealListModal from "./MealListModal.jsx";
 
-function SimpleDashboard({ calorie_total_today, calorie_total_yesterday }) {
-    const { showEffect } = useSimpleDashboard(calorie_total_today)
-    const { isTablet } = useQueries()
-    return (
-      <Grid
-        item
-        sm={12}
-        md={6}
-        className={isTablet ? "simpleDashboard-mobile" : ""}
-      >
-        <Box
-          className='card-primary'
-          sx={{
-            mb: 5,
-          }}
-        >
-          <Box
-            className={"card-primary-inner-wrapper " + showEffect}
-            sx={{ mb: 3, px: 4 }}
-          >
-            <Box
-              sx={{
-                position: "relative",
-                width: "fit-content",
-                margin: "auto",
-              }}
-              // className={showAddIcon}
-            >
-              <Typography
-                component='h4'
-                variant='h4'
-                textAlign={"center"}
-                sx={{ mt: 2, mb: 3 }}
-              >
-                Today
-              </Typography>
-            </Box>
-            <Typography
-              component='p'
-              variant='p'
-              textAlign={"center"}
-              sx={{ mb: 1 }}
-            >
-              {calorie_total_today
-                ? `${calorie_total_today} Kcal`
-                : `0000 Kcal`}
-            </Typography>
-          </Box>
-        </Box>
+function SimpleDashboard({
+  calorie_total_today,
+  calorie_total_yesterday,
+  mealsFromToday,
+}) {
+  const { showEffect } = useSimpleDashboard(calorie_total_today);
+  const { isTablet } = useQueries();
+  return (
+    <Grid
+      item
+      sm={12}
+      md={6}
+      className={isTablet ? "simpleDashboard-mobile" : ""}
+    >
+      <TodayBox
+        calorie_total_today={calorie_total_today}
+        showEffect={showEffect}
+      />
 
-        <Box className='card-secondary'>
-          <Box className='card-secondary-inner-wrapper' sx={{ px: 3, pb: 2 }}>
-            <Typography
-              component='h5'
-              variant='h5'
-              textAlign={"center"}
-              sx={{ mt: 2 }}
-            >
-              Yesterday
-            </Typography>
-            <Typography
-              component='p'
-              variant='p'
-              textAlign={"center"}
-              sx={{ mt: 2 }}
-            >
-              {calorie_total_yesterday
-                ? `${calorie_total_yesterday} Kcal`
-                : "Did you forget it again..."}
-            </Typography>
-          </Box>
-        </Box>
-      </Grid>
-    );
+      <YesterdayBox calorie_total_yesterday={calorie_total_yesterday} />
+      <MealListModal mealsFromToday={mealsFromToday} />
+    </Grid>
+  );
 }
 
 export default SimpleDashboard
